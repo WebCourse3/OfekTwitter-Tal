9dcb50110db82ec5d3a4205e3fff0217a8c4545a
tal.wit.kal <tal.wit.kal@gmail.com>
2017-10-26 11:43:05 +0300

    read the js of chat and show them. finish to do js to chat page.

--- JS/Chat.js
+++ JS/Chat.js
@@ -1,23 +1,41 @@
-function addTweet() {
-	var tweet= document.getElementById("chatBox").value;
-	document.getElementById("chatBox").value="";
+var tweetList = [
+	{username: 'Bobo', text: 'hello followers!'},
+	{username: 'Elvis', text: 'this exercise is really easy!'},
+	{username: 'Mimi', text: 'I want to go to sleep'}
+];
 
-var newTweet=document.createDocumentFragment();
-	newTweet.className='row userChat';
-var image=document.createElement('img');
-	image.className='user';
-	image.src='../Images/useravatar.png';
-	var name=document.createElement('span');
-	name.className='name';
-	name.innerText="james";
-	var brake = document.createElement('br');
-	var text=document.createElement('span');
-	text.className='name';
-	text.innerText=tweet;
+function addTweetChatBox() {
+	var tweet = document.getElementById('chatBox').value;
+	document.getElementById('chatBox').value = '';
+	var name = 'james jackson';
+	addTweet(name, tweet);
+}
+
+function addTweet(name, tweet) {
+	var newTweet = document.createElement('div');
+	newTweet.className = 'row userChat';
+	var image = document.createElement('img');
+	image.className = 'user';
+	image.src = '../Images/useravatar.png';
+	var conversation= document.createElement('div');
+	conversation.className='row conversation';
+	var nameElem = document.createElement('div');
+	nameElem.className = 'name';
+	nameElem.innerText = name;
+	var comment = document.createElement('span');
+	comment.className = 'comment';
+	comment.innerText = tweet;
 
 	newTweet.appendChild(image);
-	newTweet.appendChild(name);
-	newTweet.appendChild(brake);
-	newTweet.appendChild(text);
-	document.getElementById("tweets").appendChild(newTweet);
+	conversation.appendChild(nameElem);
+	conversation.appendChild(comment);
+	newTweet.appendChild(conversation);
+	document.getElementById('tweets').appendChild(newTweet);
+
 }
+
+(function () {
+	for (var i = 0; i < tweetList.length; i++) {
+		addTweet(tweetList[i].username, tweetList[i].text);
+	}
+}());
